6622d82bca0110babaa9aa3102f9709326ce0575
Abhimanyu <abhimanyu435@gmail.com>
2018-04-29 12:40:33 +0530

    Angular Data Binding Implementation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  loginSuccessMsg = 'Congrats! You have logged in successfully!'
+  userRating = 5;
+  userGiftStatus: boolean = false;
+  userGiftMsg = 'Gift not claimed yet!';
+  userComment = 'Sample Message';
+  
+  constructor() {
+    this.updateUserGiftStatus();
+  }
 
   ngOnInit() {
   }
 
+  getUserRating() {
+    return this.userRating;
+  }
+
+  updateUserGiftStatus() {
+    if (this.userRating == 5) {
+      this.userGiftStatus = true;
+    }
+  }
+
+  onClaimGift() {
+    if (this.userGiftStatus) {
+      this.userGiftMsg = "Gift claimed. Thanks!";
+    }
+  }
+
+  onComment(event:Event) {
+    console.log(event);
+    this.userComment = (<HTMLInputElement>event.target).value;
+  }
+  
 }
